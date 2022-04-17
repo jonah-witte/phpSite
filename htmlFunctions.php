a678f4b4c5c435a1ffd03abaeb10cb9dbe25441b
jonah-witte <90849083+jonah-witte@users.noreply.github.com>
2022-04-17 16:54:15 -0500

    Add files via upload

---
+++ htmlFunctions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+function headerFunction(){
+  echo '<h4>CSC-155-001<h4>';
+  echo '<h4> Jonah Witte </h4>';
+}
+
+function footerFunction(){
+  echo '<a href="links"> INSERT LINKS HERE </a>'; 
+}
+?>
+
+</head>
+<body>
+
+<?php
+headerFunction();
+footerFunction();
+?>
+
+</body>
+</html>
