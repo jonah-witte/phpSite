c8297ca0ff2cefa6c8a837d80b440b10f496af24
jonah-witte <90849083+jonah-witte@users.noreply.github.com>
2022-04-17 23:37:54 -0500

    Add files via upload

---
+++ phpSite155/item1.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include('library/htmlFunctions.php');
+
+if(!isset($_SESSION['username']) and !isset($_SESSION['password'])){
+  header('Location: http://www.csit.parkland.edu/~jwitte7/phpSite155/loginPage.php');
+  exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+
+<?php
+headerFunction();
+?>
+
+<p> This is a placeholder for item page ONE </p>
+
+<?php
+footerFunction();
+?>
+
+</body>
+</html>
