62900d57d7cb63b955a5783cedbbc1bed375d24f
jonah-witte <90849083+jonah-witte@users.noreply.github.com>
2022-04-18 20:02:29 -0500

    Add files via upload

--- phpSite155/library/htmlFunctions.php
+++ phpSite155/library/htmlFunctions.php
@@ -1,10 +1,23 @@
 <?php
-function headerFunction(){
+function footerFunction(){
   echo '<center>Jonah Witte CSC-155-001</center>';
 }
 
-function footerFunction(){
-  echo '<hr><center><a href="links"> INSERT FOOTER LINKS HERE </a></center>'; 
+function headerFunction(){
+  echo '<center>
+  <a href="goodbye.php"> Log Out </a> | 
+  <a href="shoppingCart.php"> ShoppingCart </a> |
+  <a href="item1.php"> ItemOne </a> |
+  <a href="item2.php"> ItemTwo </a> |
+  <a href="item3.php"> ItemThree </a> 
+  </center><hr>'; 
+}
+
+function checkSession(){
+  if(!isset($_SESSION['username']) and !isset($_SESSION['password'])){
+    header('Location: http://www.csit.parkland.edu/~jwitte7/phpSite155/loginPage.php');
+    exit;
+}
 }
 ?>
 
