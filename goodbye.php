0c600f58678234433a2620dbc0433a95850791c5
jonah-witte <90849083+jonah-witte@users.noreply.github.com>
2022-05-10 20:07:07 -0500

    Add files via upload

---
+++ goodbye.php
@@ -0,0 +1,29 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Jonah Witte
+CSC 155 -->
+
+<?php
+session_start();
+include('library/htmlFunctions.php');
+session_destroy();
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+
+
+<p> You are now logged out </p>
+<p> To log back in, click <a href="loginPage.php"> Here <a> </p>
+
+<?php
+footerFunction();
+?>
+
+</body>
+</html>
